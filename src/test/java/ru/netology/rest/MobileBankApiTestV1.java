78e1501cb5b2dd2ceb45ac6a30c4c2bbc03759fe
arthur.salahutdinov <artes_93@mail.ru>
2024-10-26 16:31:51 +0300

    обавлена JSON-схема и тест для проверки соответствия ответа

--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -9,16 +9,12 @@ class MobileBankApiTestV1 {
     @Test
     void shouldReturnDemoAccounts() {
         // Given - When - Then
-        // Предусловия
         given()
                 .baseUri("http://localhost:9999/api/v1")
-                // Выполняемые действия
                 .when()
                 .get("/demo/accounts")
-                // Проверки
                 .then()
                 .statusCode(200)
-                // Проверка соответствия ответа схеме
                 .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
     }
 }
